798efd5fa3eadacbf34c972ed796c99449f1639a
Deepak Kumar <66191666+deepakshf@users.noreply.github.com>
2023-06-01 21:32:04 +0530

    Update mlapp_sklearn_builtin_datasets.py

--- mlapp_sklearn_builtin_datasets.py
+++ mlapp_sklearn_builtin_datasets.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 31 10:25:47 2023
+
+@author: kdeep
+"""
+
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -7,7 +14,7 @@ import pandas_profiling as pf
 from streamlit_pandas_profiling import st_profile_report
 from pycaret.classification import setup, compare_models, pull, save_model, ClassificationExperiment
 from pycaret.regression import setup, compare_models, pull, save_model, RegressionExperiment 
-from sklearn import datasets
+
 
 import os
 
@@ -25,57 +32,47 @@ with st.sidebar:
     st.image(ml_logo, width=200)
     st.write('This application provides functionalities to upload datasets, perform EDA, train machine learning models using Pycaret, and download the trained models.')
     st.write('Choose your parameters.')
-    choose = st.radio('Choose your options',['Dataset', 'EDA','Training','Download'])
+    choose = st.radio('Choose you options',['Dataset', 'EDA','Training','Download'])
+    
+    
     
 if choose == 'Dataset':
-    st.write('Select a built-in dataset')
-    dataset_list = ['iris', 'boston', 'diabetes', 'wine', 'breast_cancer']  # Valid scikit-learn dataset names
-    dataset_name = st.selectbox('Select dataset', dataset_list)
-    data = getattr(datasets, dataset_name)()
-
-    if hasattr(data, 'data') and hasattr(data, 'target'):
-        df = pd.DataFrame(data.data, columns=data.feature_names)
-        target = data.target
-        df['target'] = target
-        st.dataframe(df)
+    st.write('Upload your Dataset')
+    upload = st.file_uploader('Upload here')
+    if upload:
+        data = pd.read_csv(upload,index_col=None)
+        data.to_csv("sourcev.csv", index=None)
+        st.dataframe(data)
         
-        # Save the dataset to CSV file
-        df.to_csv("sourcev.csv", index=None)
-
 if choose == 'EDA':
-    if 'df' in locals():
-        if st.button('Perform EDA'):
-            st.header("Perform profiling on the Dataset")
-            profile_report = df.profile_report()
-            st_profile_report(profile_report)
-    else:
-        st.warning('Please select a dataset first.')
+    if st.button('Perform EDA'):
+        st.header("perform profiling on Dataset")
+        profile_report = data.profile_report()
+        st_profile_report(profile_report)
     
 if choose == 'Training':
-    if 'df' in locals():
-        st.header('Start Training your model now.')
-        choice = st.sidebar.selectbox("Select your Technique", ["Regression","Classification"])
-        target = st.selectbox('Select your Target Variable', df.columns)
-        if choice == 'Classification':
-            if st.sidebar.button('Train'):
-                s1 = ClassificationExperiment()
-                s1.setup(data=df, target=target)
-                setup_data = s1.pull()
-                st.info('The setup data is as follows:-')
-                st.table(setup_data)
-
-                best_model1 = s1.compare_models()
-                compare_model = s1.pull()
-                st.info("The comparison of models is as follows:")
-                st.table(compare_model)
-
-                best_model1
-                s1.save_model(best_model1,"Machine learning Model")
-
+    st.header('Start Training your model now.')
+    choice = st.sidebar.selectbox("Select your Technique", ["Regression","Classification"])
+    target = st.selectbox('Select your Target Variable',data.columns)
+    if choice == 'Classification':
+        if st.sidebar.button('Train'):
+            s1 = ClassificationExperiment()
+            s1.setup(data=data, target=target)
+            setup_data = s1.pull()
+            st.info('The setup data is as follows:-')
+            st.table(setup_data)
+            
+            best_model1 = s1.compare_models()
+            compare_model = s1.pull()
+            st.info("The comparison of models is as follows:")
+            st.table(compare_model)
+            
+            best_model1
+            s1.save_model(best_model1,"Machine learning Model")
     if choice == 'Regression':
         if st.sidebar.button('Train'):
             s2 = RegressionExperiment()
-            s2.setup(data=df, target=target)
+            s2.setup(data=data, target=target)
             setup_data = s2.pull()
             st.info('The setup data is as follows:-')
             st.table(setup_data)
