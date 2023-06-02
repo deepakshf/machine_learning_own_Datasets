3f0fd8c2947a28f1102e7041858b77e4c8c99cf8
Deepak Kumar <66191666+deepakshf@users.noreply.github.com>
2023-06-02 12:29:07 +0530

    Update mlapp_own_datasets.py

--- mlapp_own_datasets.py
+++ mlapp_own_datasets.py
@@ -6,6 +6,24 @@ Created on Fri Mar 31 10:25:47 2023
 """
 
 import streamlit as st
+page_bg_img = f"""
+<style>
+[data-testid = "stAppViewContainer"]{{
+    background-image: url("https://img.freepik.com/free-vector/abstract-background_53876-43364.jpg?w=1060&t=st=1685685448~exp=1685686048~hmac=bf06f2136962f77d8fb9a95948390114a68a61622da7713a357e1e359c89618c");
+    background-size: cover;
+    opacity: 0.9;
+    }}
+[data-testid = "stSidebar"]{{
+    background-image: url("https://img.freepik.com/free-vector/multicolor-abstract-background_1123-53.jpg?w=740&t=st=1685685659~exp=1685686259~hmac=d3e48585afea7ba2d11c59452ad8edb5c216e26638437d1de8bbe0dec0188f72");
+    background-size: cover;
+    opacity: 0.8;
+    filter: blur(0.2px);
+    }}
+
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html = True)
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -14,6 +32,7 @@ import pandas_profiling as pf
 from streamlit_pandas_profiling import st_profile_report
 from pycaret.classification import setup, compare_models, pull, save_model, ClassificationExperiment
 from pycaret.regression import setup, compare_models, pull, save_model, RegressionExperiment 
+from sklearn import datasets
 
 
 import os
@@ -32,23 +51,30 @@ with st.sidebar:
     st.image(ml_logo, width=200)
     st.write('This application provides functionalities to upload datasets, perform EDA, train machine learning models using Pycaret, and download the trained models.')
     st.write('Choose your parameters.')
+    
     choose = st.radio('Choose you options',['Dataset', 'EDA','Training','Download'])
     
     
     
 if choose == 'Dataset':
-    st.write('Choose your Dataset')
-    available_datasets = [filename for filename in os.listdir() if filename.endswith('.csv')]
-    selected_dataset = st.selectbox('Select Dataset', available_datasets)
-    
-    if selected_dataset:
-        data = pd.read_csv(selected_dataset, index_col=None)
-        st.dataframe(data)
-        st.success('Dataset loaded successfully!')
+    dataset_name = st.sidebar.selectbox("Select Your Desired Dataset:",("Iris Dataset", "Breast Cancer Dataset", "Wine Dataset","Diabetes Dataset","Digits Dataset","Boston Housing Dataset"))
+    def get_dataset(dataset_name):
+    if dataset_name == "Iris Dataset":
+        data = datasets.load_iris()
+    elif dataset_name == "Breast Cancer Dataset":
+        data = datasets.load_breast_cancer()
+    elif dataset_name == "Wine Dataset":
+        data = datasets.load_wine()
+    elif dataset_name == "Boston Housing Dataset":
+        data = datasets.load_boston()
+    elif dataset_name == "Diabetes Dataset":
+        data = datasets.load_diabetes()
     else:
-        st.warning('No dataset available. Please upload a dataset.')
-
-
+        data = datasets.load_digits()
+        
+    data = pd.read_csv(upload,index_col=None)
+    data.to_csv("sourcev.csv", index=None)
+    st.dataframe(data)
         
 if choose == 'EDA':
     if st.button('Perform EDA'):
@@ -95,6 +121,3 @@ if choose == 'Download':
     with open("Machine_learning_Model.pkl",'rb') as f:
         st.caption("Download your model from here:")
         st.download_button("Download the file",f,"Machine_learning_Model.pkl")
-        
-        
-        
